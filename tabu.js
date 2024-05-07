fcf668ba28ae27e476bee08dbc524599cf15309e
xxx <129951174+Leozinho33@users.noreply.github.com>
2024-05-07 09:36:53 -0300

    Create tabu.js

---
+++ tabu.js
@@ -0,0 +1,83 @@
+console.log ("1 x 1 = 1 " , "1" * " 1 ");
+console.log ("1 x 2 = 2");
+console.log ("1 x 3 = 3");
+console.log ("1 x 4 = 4");
+console.log ("1 x 5 = 5");
+console.log ("1 x 6 = 6");
+console.log ("1 x 7 = 7");
+console.log ("1 x 8 = 8");
+console.log ("1 x 9 = 9");
+console.log ("1 x 10 = 10");
+
+console.log("")
+
+console.log ("3 x 1 = 3");
+console.log ("3 x 2 = 6");
+console.log ("3 x 3 = 9");
+console.log ("3 x 4 = 12");
+console.log ("3 x 5 = 15");
+console.log ("3 x 6 = 18");
+console.log ("3 x 7 = 21");
+console.log ("3 x 8 = 24");
+console.log ("3 x 9 = 27");
+console.log ("3 x 10 = 30");
+
+console.log("")
+
+console.log ("7 x 1 = 7");
+console.log ("7 x 2 = 14");
+console.log ("7 x 3 = 21");
+console.log ("7 x 4 = 28");
+console.log ("7 x 5 = 35");
+console.log ("7 x 6 = 42");
+console.log ("7 x 7 = 49");
+console.log ("7 x 8 = 56");
+console.log ("7 x 9 = 63");
+console.log ("7 x 10 = 70");
+
+console("")
+
+let T = 32;
+
+console.log ( T + "x 1 = ", T * 1 );
+console.log ( T + "x 2 = ", T * 2 );
+console.log ( T + "x 3 = ", T * 3 );
+console.log ( T + "x 4 = ", T * 4 );
+console.log ( T + "x 5 = ", T * 5 );
+console.log ( T + "x 6 = ", T * 6 );
+console.log ( T + "x 7 = ", T * 7 );
+console.log ( T + "x 8 = ", T * 8 );
+console.log ( T + "x 9 = ", T * 9 );
+console.log ( T + "x 10 = ", T * 10 );
+
+T=T +1;
+
+
+
+console.log ( T + "x 1 = ", T * 1 );
+console.log ( T + "x 2 = ", T * 2 );
+console.log ( T + "x 3 = ", T * 3 );
+console.log ( T + "x 4 = ", T * 4 );
+console.log ( T + "x 5 = ", T * 5 );
+console.log ( T + "x 6 = ", T * 6 );
+console.log ( T + "x 7 = ", T * 7 );
+console.log ( T + "x 8 = ", T * 8 );
+console.log ( T + "x 9 = ", T * 9 );
+console.log ( T + "x 10 = ", T * 10 );
+
+console("");
+
+T ++ ;
+
+console("");
+
+console.log ( T + "x 1 = ", T * 1 );
+console.log ( T + "x 2 = ", T * 2 );
+console.log ( T + "x 3 = ", T * 3 );
+console.log ( T + "x 4 = ", T * 4 );
+console.log ( T + "x 5 = ", T * 5 );
+console.log ( T + "x 6 = ", T * 6 );
+console.log ( T + "x 7 = ", T * 7 );
+console.log ( T + "x 8 = ", T * 8 );
+console.log ( T + "x 9 = ", T * 9 );
+console.log ( T + "x 10 = ", T * 10 );
